6430e3d524afef415ff9ac2e03d2ad6b45c79c64
Ira Shturyn <ira.shturyn@gmail.com>
2018-12-17 02:38:05 +0200

    renamed method

--- taskWithTrain/src/main/java/com/cintavra/Main.java
+++ taskWithTrain/src/main/java/com/cintavra/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         Service service = new Service();
-        service.ticketOrder();
+        service.orderTicket();
     }
 }
