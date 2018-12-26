90a8e9153d7151a17fc9c2de1ca8b4a62b299b2e
Ira Shturyn <ira.shturyn@gmail.com>
2018-12-26 22:12:08 +0200

    add to Main exception handling method

--- taskWithTrain/src/main/java/com/cintavra/Main.java
+++ taskWithTrain/src/main/java/com/cintavra/Main.java
@@ -2,64 +2,99 @@ package com.cintavra;
 
 import com.cintavra.Train.*;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanInt = new Scanner(System.in);
+        Scanner scanString = new Scanner(System.in);
         Service service = new Service();
-        Scanner scan = new Scanner(System.in);
+
         int choice;
         do {
             System.out.println("Enter start station:");
-            startStation = scan.nextLine();
+            String startStation = scanString.nextLine();
 
             System.out.println("Enter final station:");
-            endStation = scan.nextLine();
+            String endStation = scanString.nextLine();
 
-            if (searchAppropriateTrain().isEmpty()) {
+            List<Train> trainsForUser = service.findTrainsForUser(startStation, endStation);
+            if (trainsForUser.isEmpty()) {
                 System.out.println("There is no train with such route.");
             } else {
 
-                showAllPlaceInTrain();
+                service.checkingForOtherPlaces(trainsForUser);
+                service.showAllPlaceInTrains(trainsForUser);
+
+                System.out.println("Choose your place");
 
                 System.out.println("Enter number of train:");
-                trainNumber = scan.nextInt() - 1;
+                int trainNumber = scanInt.nextInt();
 
                 System.out.println("Enter number of coach:");
-                coachNumber = scan.nextInt() - 1;
+                int coachNumber = scanInt.nextInt();
 
                 System.out.println("Enter number of place:");
-                placeNumber = scan.nextInt() - 1;
+                int placeNumber = scanInt.nextInt();
 
-                //перевірка на існування даних за значеннями введених користувачем
-                if (checkEnteredData()) {
+                if (service.checkEnteredData(trainNumber, coachNumber, placeNumber, trainsForUser)) {
                     System.out.println();
                     System.out.println("Print your name:");
-                    scan.nextLine();
-                    userName = scan.nextLine();
-                    System.out.println();
+                    String userName = scanString.nextLine();
 
-                    System.out.println("Ticket info:");
-                    System.out.println(" Train number: " + (trainNumber + 1) + "\n Number of coach: " + (coachNumber + 1) +
-                            "\n Place number: " + (placeNumber + 1) + "\n Customer: " + userName);
                     System.out.println("\nTo confirm your order enter 1");
-                    choice = scan.nextInt();
-
-                    if (choice == 1) {
-                        buyPlace();
-                        removeTrainsForClient();
-                        System.out.println("Purchase successful!");
-                    } else {
-                        checkTicketState();
+                    while (true) {
+                        String stringChoice = scanString.nextLine();
+                        if (!isIntEntered(stringChoice)) {
+                            cleanConsole();
+                            System.out.println("Error");
+                            System.out.println("\nTo confirm your order enter 1");
+                        } else {
+                            choice = Integer.parseInt(stringChoice);
+                            break;
+                        }
                     }
+
+                    if (choice == 1)
+                        service.buyTicket(userName, trainNumber, coachNumber, placeNumber);
+                    System.out.println("Purchase successful!");
+                    service.checkPlaceState(trainsForUser, coachNumber);
                 } else {
                     System.out.println("Incorrect data!\nTry again.");
                 }
             }
             System.out.println("\nTo buy another ticket enter 1");
-            choice = scan.nextInt();
-            scan.nextLine();
+            while (true) {
+                String stringChoice = scanString.nextLine();
+                if (!isIntEntered(stringChoice)) {
+                    cleanConsole();
+                    System.out.println("Error");
+                    System.out.println("\nTo buy another ticket enter 1");
+                } else {
+                    choice = Integer.parseInt(stringChoice);
+                    break;
+                }
+            }
         } while (choice == 1);
     }
+
+    private static boolean isIntEntered(String str) {
+        int i;
+        boolean result = false;
+        try {
+            i = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return result;
+        }
+        if (i == 1) {
+            result = true;
+        }
+        return result;
+    }
+
+    private static void cleanConsole() {
+    }
+
 }
